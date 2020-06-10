9240d32da5432608f1f53b717e06f57f8b3b2edf
drshariqalikhan <speromedtech@gmail.com>
2020-06-10 11:28:25 +0800

    fetch 01

--- lib/fecthdata.dart
+++ lib/fecthdata.dart
@@ -3,28 +3,51 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Photo>> fetchPhotos(http.Client client) async {
+Future<List<CovidData>> fetchCovidList(http.Client client) async {
   final response =
       await client.get('https://jsonplaceholder.typicode.com/photos');
 
   // Use the compute function to run parsePhotos in a separate isolate.
   return compute(parseCovid, response.body);
 
-  // return compute(parsePhotos, response.body);
 }
 
-// // A function that converts a response body into a List<Photo>.
-// List<Photo> parsePhotos(String responseBody) {
-//   final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+// // A function that converts a response body into a List<CovidData>.
 
-//   return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
-// }
 
 
 List<CovidData> parseCovid(String responseBody){
   final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
   return parsed.map<CovidData>((json) => CovidData.fromJson(json)).toList();
+}
+
+class CovidData {
+  String date;
+  String time;
+  String place;
+  double lat;
+  double lon;
+
+  CovidData({this.date, this.time, this.place, this.lat, this.lon});
+
+  CovidData.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    time = json['time'];
+    place = json['place'];
+    lat = json['lat'];
+    lon = json['lon'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['date'] = this.date;
+    data['time'] = this.time;
+    data['place'] = this.place;
+    data['lat'] = this.lat;
+    data['lon'] = this.lon;
+    return data;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'fecthdata.dart';
 import 'homepage.dart';
 
-void main() {
+
+List d ;
+void main()async {
+  d = await fetchCovidList(http.Client());
+
   runApp(MyApp());
+  // print(d);
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  
+
   
   @override
   Widget build(BuildContext context) {
+   int z  = d.length;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -20,61 +27,9 @@ class MyApp extends StatelessWidget {
        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(msg: 'hi',scannerWidget: Card(child: Text('placeholder'),),),
+      home: MyHomePage(msg: '${z.toString()}',scannerWidget: Card(child: Text('placeholder'),),),
     );
   }
 }
 
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-     
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return Scaffold(
-//       appBar: AppBar(
-       
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-        
-//         child: Column(
-          
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many :',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Incr',
-//         child: Icon(Icons.add),
-//       ), 
-//     );
-//   }
-// }
